75a4475b3c3f2bdf68c418a8f8e898e74bb7b690
anders-g-hansen <anders.g.hansen@medtronic.com>
2015-09-21 06:43:29 -0500

    A new attempt at implementing a mobile "root" element.  This uses a pseudo MobileElement that delegates to the driver instead of forcing a find like we were previously doing.

--- src/main/groovy/geb/mobile/GebMobileNavigatorFactory.groovy
+++ src/main/groovy/geb/mobile/GebMobileNavigatorFactory.groovy
@@ -3,7 +3,8 @@ package geb.mobile
 import geb.Browser
 import geb.navigator.Navigator
 import geb.navigator.factory.AbstractNavigatorFactory
-import io.appium.java_client.MobileBy
+import io.appium.java_client.FindsByAndroidUIAutomator
+import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import org.openqa.selenium.WebElement
 
@@ -28,12 +29,34 @@ class GebMobileNavigatorFactory extends AbstractNavigatorFactory{
             // Another option here may be to set an empty list.  Then in the AndroidUIAutomatorNonEmptyNavigator detect
             // an empty list and send commands against the browser instead of the context.  If we do this we need to
             // override the isEmpty on AndroidUIAutomatorNonEmptyNavigator to always return false
-            list = browser.driver.findElements(MobileBy.AndroidUIAutomator("new UiSelector().instance(0)")) as List
+//            list = browser.driver.findElements(MobileBy.AndroidUIAutomator("new UiSelector().instance(0)")) as List
+
+            // This is another attempt at making an root element.  It users a wrapper element which delegates to the
+            // driver itself instead of forcing a find like we were previously doing.
+            list = Arrays.asList(new AndroidRootElement(browser.driver))
         } else {
             list = browser.driver.findElementsByXPath("//*") as List
         }
 
         createFromWebElements(list)
+    }
+
+    public class AndroidRootElement extends MobileElement implements FindsByAndroidUIAutomator {
+        AndroidDriver driver
+
+        AndroidRootElement(AndroidDriver driver) {
+            this.driver = driver
+        }
+
+        @Override
+        public WebElement findElementByAndroidUIAutomator(String using) {
+            driver.findElementByAndroidUIAutomator(using);
+        }
+
+        @Override
+        public List<WebElement> findElementsByAndroidUIAutomator(String using) {
+            driver.findElementsByAndroidUIAutomator(using);
+        }
     }
 
 }
